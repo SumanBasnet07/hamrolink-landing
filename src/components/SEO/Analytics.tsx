ca70fccdae9898ab944f199b618a9081af18779a
SumanBasnet07 <sumanbas640@gmail.com>
2026-04-27 19:20:00 +0900

    feat: add Analytics component with GA4, Plausible, and Meta Pixel tracking

--- src/components/SEO/Analytics.tsx
+++ src/components/SEO/Analytics.tsx
@@ -12,6 +12,7 @@ export function Analytics() {
     const handleCTAClick = (e: MouseEvent) => {
       const target = (e.target as HTMLElement).closest('a');
       if (target && target.href && target.href.includes('app.hamrolink.com')) {
+        console.log('InitiateCheckout CTA clicked!');
         if (typeof window !== 'undefined' && (window as any).fbq) {
           (window as any).fbq('track', 'InitiateCheckout');
         }
@@ -29,16 +30,16 @@ export function Analytics() {
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
         strategy="afterInteractive"
       />
-      <Script id="ga4-init" strategy="afterInteractive">
-        {`
+      <Script id="ga4-init" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GA_ID}', {
             page_path: window.location.pathname,
           });
-        `}
-      </Script>
+        `
+      }} />
       
       {/* Plausible (Privacy-focused) */}
       <Script
@@ -52,8 +53,8 @@ export function Analytics() {
       />
       
       {/* Meta Pixel Code */}
-      <Script id="meta-pixel" strategy="afterInteractive">
-        {`
+      <Script id="meta-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -64,8 +65,8 @@ export function Analytics() {
           'https://connect.facebook.net/en_US/fbevents.js');
           fbq('init', '${FB_PIXEL_ID}');
           fbq('track', 'PageView');
-        `}
-      </Script>
+        `
+      }} />
       <noscript>
         <img height="1" width="1" style={{ display: 'none' }}
           src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
